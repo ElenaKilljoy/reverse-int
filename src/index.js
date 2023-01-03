773c7c2286dcb65b980c0c81c5305cac41c335da
ElenaKilljoy <lenokcardio@gmail.com>
2023-01-03 23:00:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  let s = n.toString();
+  let i = s.length - 1;
+  let result = '';
+  while (i >= 0) {
+    if (s[i] !== '-') {
+      result = result + s[i];
+      i = i - 1;
+    } else {
+      i = i - 1;
+    }
+  }
+  return result * 1;
 }
